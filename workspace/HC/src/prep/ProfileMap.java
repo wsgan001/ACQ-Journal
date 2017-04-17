9c795699afbedb882c64549c75419eb1d2c0f712
CYK <yankaichan3@gmail.com>
2017-04-17 16:26:00 +0800

    no message

---
+++ workspace/HC/src/prep/ProfileMap.java
@@ -0,0 +1,16 @@
+package prep;
+
+import java.util.Map;
+
+import config.Config;
+
+public class ProfileMap {
+	public Map<String, String> map=null;
+	
+	public void readFile(String fileName){
+		String name=Config.localPath;
+		
+	}
+	
+	
+}
