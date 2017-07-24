8c34a652038fe13a31a5e06a4ebdb9089f59b5cd
CYK <yankaichan3@gmail.com>
2017-07-24 22:10:10 +0800

    no message

--- workspace/HC/src/algorithm/FPTreeAlgo/MFPTree.java
+++ workspace/HC/src/algorithm/FPTreeAlgo/MFPTree.java
@@ -97,7 +97,6 @@ public void insert(List<Integer> itemset,int itemsetLen,int support){
 				currentNode=child;
 		 } 
 	 } 
-	 System.out.println("insert done");
 }
 	 
 //maintain two Map:itemNodeMap and itemLastNodeMap
