2fb431efd1d6357190069ea60db41dd6ff8a277b
CYK <yankaichan3@gmail.com>
2018-03-07 10:52:31 +0800

    no message

--- CLionWorkspace/test/library.cpp
+++ CLionWorkspace/test/library.cpp
@@ -2,18 +2,23 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int value(){
 
+    int a[3][4]={0,1,2,3,4,5,6,7,8,9,10,11};
+    int (*p)[4] = &a[1];  cout << ** p << endl;
+    for (int *q = *p;q!=*p+4;q++) cout<< *q <<endl;
 
+//    for(int *p1 = p+4;p!=p1;p++) cout<< *p <<endl;
+
+    return 0;
 }
 
 
 int main() {
-    string s ;
-    cin>>s;
-    cout<<s<<endl;
+    value();
 
     return 0;
 }
