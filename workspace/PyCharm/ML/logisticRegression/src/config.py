86335142308da60c00fea1c50f36b02413ee5c98
CYK <yankaichan3@gmail.com>
2018-05-16 17:03:44 +0800

    logistic Regression finished

---
+++ workspace/PyCharm/ML/logisticRegression/src/config.py
@@ -0,0 +1,4 @@
+testFile = "/Users/chenyankai/gitRepository/workspace/PyCharm/ML/logisticRegression/file/testSet.txt"
+horseTrain = "/Users/chenyankai/gitRepository/workspace/PyCharm/ML/logisticRegression/file/horseColicTraining.txt"
+horseTest = "/Users/chenyankai/gitRepository/workspace/PyCharm/ML/logisticRegression/file/horseColicTest.txt"
+
---
+++ workspace/PyCharm/ML/logisticRegression/src/test.py
@@ -0,0 +1,10 @@
+from src.logRegres import *
+from src.horseHealth import *
+
+
+# dataArr, labelMat = loadDataSetDemo()
+# weights = gradAscent(dataArr, labelMat)
+# weights = stocGradAscent0(dataArr, labelMat)
+# plotBestFit(weights, dataArr, labelMat)
+
+multiTest()
